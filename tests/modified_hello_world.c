a193242e970c81e93b66365dae8c1213ee3256e7
qwavies <qwavsbusiness@gmail.com>
2025-11-24 18:34:39 +1100

    Added a modified hello world file with no semicolons

---
+++ tests/modified_hello_world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+   printf("Hello, world!\n") printf("2 print statements on the same line??\n")
+   printf("Wow! No semicolons in this file???\n")
+   return 0;
+}
